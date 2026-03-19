9c3b686c6a4ec686c0a4325a126af955fbd325e9
joaolima159 <jvlemosdelima@hotmail.com>
2026-03-19 14:09:21 -0300

    Mudanca frases na Home

---
+++ js/avaliacao.js
@@ -0,0 +1,21 @@
+
+/*
+//Controla o modal da Shopee
+
+document.querySelector('.shopee-link').addEventListener('click', openShopeeModal);
+document.querySelector('.modal-close').addEventListener('click', closeShopeeModal);
+document.querySelector('.shopee-modal').addEventListener('click', (e) => {
+  if (e.target.classList.contains('shopee-modal')) closeShopeeModal();
+});
+
+function openShopeeModal() {
+  document.querySelector('.shopee-modal').classList.add('active');
+  document.body.style.overflow = 'hidden';
+}
+
+function closeShopeeModal() {
+  document.querySelector('.shopee-modal').classList.remove('active');
+  document.body.style.overflow = '';
+}
+
+*/
